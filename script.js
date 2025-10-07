24cd391b98846fe125a8cf246d0490a3e7463142
jihanEzzz <j4941066@gmail.com>
2025-10-07 12:03:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 const tombolSapa = document.getElementByld('sapaButton')
 //2. Tombohkan 'event listener'untuk aksi'click'
 tombolSapa.addEventListener('click',function){
-  //3. Jalankan kode ini ketika tombol di-klik
-  alert('Termakasih masih sayang aku!'):
+//3. Jalankan kode ini ketika tombol di-klik
+alert('Termakasih masih sayang aku!'):
 }):
