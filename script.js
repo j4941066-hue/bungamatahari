77ce807e24d94c1679aede31c4a0048690652dcb
jihanEzzz <j4941066@gmail.com>
2025-10-10 08:49:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ tombolSapa.addEventListener('click', function() {
     // 3. Jalankan kode ini ketika tombol di-klik
     alert('Terima kasih masih sayang aku!');
 });
+
+const startButton = document.getElementById('start-button');
+const menuContainer = document.querySelector('.menu-container');
+
+startButton.addEventListener('click', () => {
+  // Menambah atau menghapus kelas 'active'
+  menuContainer.classList.toggle('active');
+});
